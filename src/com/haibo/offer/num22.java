8e40c77aa9e9168fc2862945576ba28c9d46f15a
haibo <540450460@qq.com>
2018-07-15 21:48:50 +0800

    剑指offer17

---
+++ src/com/haibo/offer/num22.java
@@ -0,0 +1,34 @@
+package com.haibo.offer;
+
+import java.util.Stack;
+
+/**
+ * Created with IDEA.
+ * User:haibo.
+ * DATE:2018/7/15/015
+ * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。
+ * 假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4，5,3,2,1是该压栈序列对应的一个弹出序列，
+ * 但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+ */
+public class num22 {
+    //借用一个辅助的栈，push序列依次入栈，每次都判断，栈顶元素和pop序列是否相等，相等则弹出栈，
+    // 不相等，则push序列继续入栈，最后判断栈是否为空
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        if(pushA.length == 0 || popA.length == 0)
+            return false;
+        Stack<Integer> s = new Stack<Integer>();
+        //用于标识弹出序列的位置
+        int popIndex = 0;
+        for(int i = 0; i< pushA.length;i++){
+            s.push(pushA[i]);
+            //如果栈不为空，且栈顶元素等于弹出序列
+            while(!s.empty() &&s.peek() == popA[popIndex]){
+                //出栈
+                s.pop();
+                //弹出序列向后一位
+                popIndex++;
+            }
+        }
+        return s.empty();
+    }
+}
