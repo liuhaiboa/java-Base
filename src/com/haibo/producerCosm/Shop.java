458defa358332ef3e13bd3ad9c3344c693c6ad24
haibo <540450460@qq.com>
2018-07-22 21:01:31 +0800

    生产者消费者模型2

--- src/com/haibo/producerCosm/Shop.java
+++ src/com/haibo/producerCosm/Shop.java
@@ -92,13 +92,13 @@ class Customer implements Runnable{
     }
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+
         while(true){
             shop.sale();
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
+
                 e.printStackTrace();
             }
         }
